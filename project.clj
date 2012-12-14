0bbc1ba7798650c9417df0ccc3e16f238df3ebf9
Thom Lawrence <thom@hotwoofy.com>
2012-12-14 20:59:43 +0000

    Use an ordered map for columns, to maintain the order specified in the migration.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.1.1"]
-                 [org.clojure/tools.macro "0.1.1"]]
+                 [org.clojure/tools.macro "0.1.1"]
+                 [org.flatland/ordered "1.4.0"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              ;:1.5 {:dependencies [[org.clojure/clojure "1.5.0-beta1"]]}
              :dev
